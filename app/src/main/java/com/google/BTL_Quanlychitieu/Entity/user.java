35d9398a6635d9d6e43a7a79d29277bfa7e66a03
Tuanvietcode <phungtuan12a1@gmail.com>
2024-04-10 19:36:14 +0700

    update

--- app/src/main/java/com/google/BTL_Quanlychitieu/Entity/user.java
+++ app/src/main/java/com/google/BTL_Quanlychitieu/Entity/user.java
@@ -1,3 +1,17 @@
 package com.google.BTL_Quanlychitieu.Entity;
+import java.io.Serializable;
 
+public class user implements Serializable {
+    public String name;
+    public String username;
+    public String Pass;
+    public String avatar;
+
+    public user(String name, String username, String pass, String avatar) {
+        this.name = name;
+        this.username = username;
+        this.Pass = pass;
+        this.avatar = avatar;
+    }
+}
 
